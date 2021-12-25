b0fae3375b64bd82e3e55daaac72f8b373a48cd4
jsilva83 <jorge.h.silva@gmail.com>
2021-12-25 09:53:43 +0000

    Tweak and prepared to different dictionaries. Added a dictionary of English words (2800 most common).

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ import pandas as pd
 import random
 
 # Global variables.
-global french_words_df
+global dictionary_words_df
 global card_canvas
-global a_text_canvas
-global b_text_canvas
+global title_text_canvas
+global center_text_canvas
+global down_text_canvas
 global card_nr
 global root_window
 global x_image
@@ -18,7 +19,12 @@ global card_front_image
 global scheduled_job_id
 
 # Constants.
-BACKGROUND_COLOR = "#B1DDC6"
+BACKGROUND_COLOR = '#B1DDC6'
+ORIGINAL_WORD_COLOR = '#f62763'
+DICT_ORIGINAL_LANGUAGE = 'English'
+DICT_TARGET_LANGUAGE = 'Portuguese'
+FILE_DICT_REMAINING_WORDS = './data/words_to_learn.csv'
+FILE_DICT_INITIAL_WORDS = './data/2800_english_words.csv'
 
 
 def flip_card():
@@ -26,18 +32,25 @@ def flip_card():
     global canvas_image_item
     global card_back_image
     global card_front_image
+    global title_text_canvas
+    global center_text_canvas
+    global down_text_canvas
     card_canvas.itemconfig(canvas_image_item, image=card_back_image)
-    card_canvas.itemconfig(a_text_canvas, text='English', fill='white')
-    card_canvas.itemconfig(b_text_canvas, text=french_words_df.at[card_nr, 'English'], fill='white')
+    card_canvas.itemconfig(title_text_canvas, text=DICT_TARGET_LANGUAGE, fill='white')
+    card_canvas.itemconfig(center_text_canvas, text=dictionary_words_df.at[card_nr, DICT_TARGET_LANGUAGE], fill='white')
+    card_canvas.itemconfig(down_text_canvas,
+                           text=dictionary_words_df.at[card_nr, DICT_ORIGINAL_LANGUAGE],
+                           fill=ORIGINAL_WORD_COLOR,
+                           )
     return
 
 
 def flash_card(button_pressed):
     """Returns a random card and updates the canvas."""
-    global french_words_df
+    global dictionary_words_df
     global card_canvas
-    global a_text_canvas
-    global b_text_canvas
+    global title_text_canvas
+    global center_text_canvas
     global card_nr
     global root_window
     global scheduled_job_id
@@ -48,44 +61,46 @@ def flash_card(button_pressed):
     # If the user pressed the right button then remove the word / row from dataframe.
     # If it is initiating, then do not execute.
     if button_pressed != 'ini' and button_pressed == 'right':
-        french_words_df.drop(
+        dictionary_words_df.drop(
             labels=[card_nr],  # list of indexes to delete.
             axis=0,  # 0 means rows, 1 means columns.
             inplace=True,  # update the dataframe.
         )
         # Reindexing the remaining rows to start from 0 to size of df.
-        french_words_df.reset_index(
+        dictionary_words_df.reset_index(
             drop=True,
             inplace=True,
         )
     # Select next card and update the gui.
-    card_deck_size = french_words_df['French'].size
+    card_deck_size = dictionary_words_df[DICT_ORIGINAL_LANGUAGE].size
     card_nr = random.randint(0, card_deck_size - 1)
-    french_word = french_words_df.at[card_nr, 'French']
+    original_word = dictionary_words_df.at[card_nr, DICT_ORIGINAL_LANGUAGE]
     card_canvas.itemconfig(canvas_image_item, image=card_front_image)
-    card_canvas.itemconfig(a_text_canvas, text='French', fill='black')
-    card_canvas.itemconfig(b_text_canvas, text=french_word, fill='black')
+    card_canvas.itemconfig(title_text_canvas, text=DICT_ORIGINAL_LANGUAGE, fill='black')
+    card_canvas.itemconfig(center_text_canvas, text=original_word, fill='black')
+    card_canvas.itemconfig(down_text_canvas, text='', fill=ORIGINAL_WORD_COLOR)
     # Launch a new job to flip the card after 3000 millisenconds.
-    scheduled_job_id = root_window.after(3000, flip_card)
+    scheduled_job_id = root_window.after(5000, flip_card)
     return
 
 
 def on_closing():
     """What to do when closing the application.\nWhen closing the applications write the unkown words to a file."""
-    global french_words_df
+    global dictionary_words_df
     global root_window
     # Write unknown words to a file.
-    french_words_df.to_csv('./data/words_to_learn.csv', index=False)
+    dictionary_words_df.to_csv(FILE_DICT_REMAINING_WORDS, index=False)
     root_window.destroy()
     return
 
 
 def app_gui():
     """Creates the gui and controls its execution."""
-    global french_words_df
+    global dictionary_words_df
     global card_canvas
-    global a_text_canvas
-    global b_text_canvas
+    global title_text_canvas
+    global center_text_canvas
+    global down_text_canvas
     global root_window
     global x_image
     global v_image
@@ -110,8 +125,9 @@ def app_gui():
     card_canvas = tkinter.Canvas(root_window)
     card_canvas.config(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
     canvas_image_item = card_canvas.create_image(0, 0, image=card_front_image, anchor='nw')
-    a_text_canvas = card_canvas.create_text(400, 150, text='Title', fill='black', font=('Ariel', 40, 'italic'))
-    b_text_canvas = card_canvas.create_text(400, 263, text='word', fill='black', font=('Ariel', 60, 'bold'))
+    title_text_canvas = card_canvas.create_text(400, 150, text='Title', fill='black', font=('Ariel', 40, 'italic'))
+    center_text_canvas = card_canvas.create_text(400, 263, text='word', fill='black', font=('Ariel', 60, 'bold'))
+    down_text_canvas = card_canvas.create_text(400, 363, text='', fill='black', font=('Ariel', 50, 'italic'))
     card_canvas.grid(row=0, column=0, columnspan=2)
     # Create wrong button.
     wrong_button = tkinter.Button(root_window)
@@ -135,9 +151,9 @@ def app_gui():
     right_button.grid(row=1, column=1)
     # Read the cards from file from words_to_learn if it exists.
     try:
-        french_words_df = pd.read_csv('./data/words_to_learn.csv')
+        dictionary_words_df = pd.read_csv(FILE_DICT_REMAINING_WORDS)
     except FileNotFoundError:
-        french_words_df = pd.read_csv('./data/french_words.csv')
+        dictionary_words_df = pd.read_csv(FILE_DICT_INITIAL_WORDS)
     # Start the game by flashing the cards.
     flash_card('init')
     # Wait for events.
